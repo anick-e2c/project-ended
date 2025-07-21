037f249f5e87cfc201f7edb466db06de5a14674f
koueAnicet <anicetkoue@gmail.com>
2025-07-21 11:21:17 +0000

    configuration details sur un user à droite

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -25,7 +25,7 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
             <img src={assets.help_icon} alt=""  className='max-md:hidden max-w-5'/>
         </div>
       {/*---------chat area-------*/}
-      <div className=' flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
+      <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
         { messagesDummyData.map((msg, index)=>(
           <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9'
             && 'flex-row-reverse'}`}>
