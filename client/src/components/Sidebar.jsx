7bffc88662b360384415a49b76be1afb42fd609a
koueAnicet <anicetkoue@gmail.com>
2025-07-21 09:44:32 +0000

    Configuration de zone de discussion

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import assets, { userDummyData } from '../assets/assets1'
 import { useNavigate } from 'react-router-dom'
 
-const Sidebar = ({ selectedUSer, SetSelectedUser}) => {
+const Sidebar = ({ selectedUser, setSelectedUser}) => {
     const navigage = useNavigate()
   return (
-    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUSer ? "max-md:hidden" : ''}`}>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ''}`}>
         <div className='pb-5'> 
             <div className='flex justify-between items-center'>
                 <img src={ assets.logo } alt="logo" className='max-w-40' /> 
@@ -30,9 +30,9 @@ const Sidebar = ({ selectedUSer, SetSelectedUser}) => {
         {/* -----User List----- */}
         <div className='flex flex-col'>
             { userDummyData.map((user, index) => (
-                <div onClick={()=> {SetSelectedUser(user)}}
+                <div onClick={()=> {setSelectedUser(user)}}
                     key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer 
-                    max-sm:text-sm ${selectedUSer?._id === user._id && 'bg-[#282142]/50'}`}
+                    max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}
                 >
                     <img src={user?.profilePic || assets.avatar_icon} alt="" 
                     className='w-[35px] aspect-[1/1] rounded-full'/>
@@ -41,7 +41,7 @@ const Sidebar = ({ selectedUSer, SetSelectedUser}) => {
                         {
                             index < 3
                             ? <span className='text-green-400 text-xs'>Online</span>
-                            : <span className='text-green-400 text-xs'>Offline</span>
+                            : <span className='text-gray-600 text-xs'>Offline</span>
                         }
                     </div>
                     { index > 2 && 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,9 +12,9 @@ const Home = () => {
             overflow-hidden h-[100%]  grid grid-cols-1 relative ${selectedUser ? 
             'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr' : 'md:grid-cols-2'}`
         }>
-            <Sidebar />
-            <ChatContainer />
-            <RightSidebar />
+            <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+            <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+            <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
         </div>
         <h1>Home page</h1>
     </div>
