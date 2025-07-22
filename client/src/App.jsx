32139e1d10b1cddc46844a7a197176d97bb12dd7
koueAnicet <anicetkoue@gmail.com>
2025-07-22 23:56:06 +0000

    Configuration de context et erreur

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, {useContext} from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
-import Profile from './pages/Profile.jsx'
+import Profile from './pages/Profile.jsx';
+import {Toaster } from 'react-hot-toast';
+import { AuthContext } from '../context/AuthContextContex.jsx';
+
 function App() {
+const { authUser }= useContext(AuthContext);
+
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-cover bg-no-repeat">
+      <Toaster />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login"/>} />
+        <Route path="/login" element={!authUser ?<Login /> : <Navigate to="/"/>} />
+        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login"/>} />
         {/* Add more routes as needed */}
        
       </Routes>
