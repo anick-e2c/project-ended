388070b5b31cb7774a26e5b3bc9999e0f10dbb18
koueAnicet <anicetkoue@gmail.com>
2025-07-22 14:20:02 +0000

    Configuration de controller de mise à jour du profile

---
+++ server/routes/userRoute.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { checkAuth, login, signup, updateProfile } from '../controllers/userController';
+import { protectRoutes } from '../middleware/auth';
+
+
+const userRouter = express.Router();
+
+userRouter.post('/signup', signup);
+userRouter.post('/login', login);
+userRouter.post('/signup', signup);
+userRouter.post('/update-profile', protectRoutes, updateProfile);
+userRouter.post('/check', protectRoutes, checkAuth);
+
+
+export default userRouter;
